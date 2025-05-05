444d3ce6b2a40eaa741f212e04e4205b6f1a8460
mangimangi <holla@mangi.dev>
2025-05-05 15:17:20 -0500

    fix fonts

--- generate_site.py
+++ generate_site.py
@@ -9,9 +9,7 @@ TEMPLATE_HEADER = '''<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <title>{title}</title>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Quicksand&family=Solway:wght@700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.typekit.net/fjf5mmt.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <link rel="stylesheet" href="styles.css">
 </head>
